a26e1c35a75a4aebc39266f094da567b7d448731
Alam, Mahbub (Agoda) <mahbub.alam@agoda.com>
2017-05-18 14:42:02 +0700

    Modified solution to codeforce problem

--- src/main/scala/Solution.java
+++ src/main/scala/Solution.java
@@ -1,37 +1,37 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by malam on 5/17/17.
  */
-class Node{
-    private boolean visited;
-    private int count;
-
-    public Node(boolean visited) {
-        this.visited = visited;
-        this.count = 0;
-    }
-
-    public void setVisited(boolean b) { this.visited = b; }
-    public boolean getVisited() { return this.visited; }
-    public void inrementCount() { this.count++; }
-    public void decrementCount() { this.count--; }
-    public int getCount() { return this.count; }
-}
 
 public class Solution {
 
+    static class Node{
+        private boolean visited;
+        private int count;
+
+        public Node(boolean visited) {
+            this.visited = visited;
+            this.count = 0;
+        }
+
+        public void setVisited(boolean b) { this.visited = b; }
+        public boolean getVisited() { return this.visited; }
+        public void inrementCount() { this.count++; }
+        public void decrementCount() { this.count--; }
+        public void resetCount() { this.count = 0; }
+        public int getCount() { return this.count; }
+    }
 
     /**
      * @param str Search string
      * @return Set of unique characters in string
      */
-    public static Set<String> uniqueChars(String str) {
-        Set<String> result = new HashSet<>();
+    public static Map<String, Node> uniqueChars(String str) {
+        Map<String, Node> result = new HashMap<>();
 
         for (char c : str.toCharArray()) {
-            result.add(String.valueOf(c));
+            result.put(String.valueOf(c), new Node(false));
         }
 
         return result;
@@ -46,49 +46,77 @@ public class Solution {
      * Pattern = BCDae
      * >> first character 'B' only appears once, so stop and return pattern
      * @param str Pattern found
-     * @param charset Character set
+     * @param uniqueChars Character set
      * @return Optimized string
      */
-    public static String optimize(String str, Node[] charset) {
+    public static String optimize(String str, Map<String, Node> uniqueChars) {
         int start = 0;
-        while(charset[(int)str.charAt(start)].getCount() > 1) {
-            charset[(int)str.charAt(start)].decrementCount();
+        while(uniqueChars.get(String.valueOf(str.charAt(start))).getCount() > 1) {
+            uniqueChars.get(String.valueOf(str.charAt(start))).decrementCount();
             start++;
         }
         return str.substring(start);
     }
 
-    public static void main(String[] args) {
-        String str = "CBaBaBCCe";
-
-        Set<String> uniqueChars = uniqueChars(str);
-        int uniqueCharsLength = uniqueChars.size();
-        Node[] charset = new Node[256];
-
-        // initialize node
-        for (int i=0; i<256; i++) {
-            charset[i] = new Node(false);
-        }
+    public static void findMinRooms(String str) {
+        Map<String, Node> uniqueChars = uniqueChars(str);
+        int uniqueCharsLength = uniqueChars.keySet().size();
 
         int count = 0;
+        int start = 0;
         StringBuilder sb = new StringBuilder();
+        List<String> patterns = new ArrayList<>();
         for (int i=0; i<str.length(); i++) {
-            if (!charset[(int)str.charAt(i)].getVisited()) {
-                charset[(int)str.charAt(i)].setVisited(true);
-                charset[(int)str.charAt(i)].inrementCount();
+            if (!uniqueChars.get(String.valueOf(str.charAt(i))).getVisited()) {
+                uniqueChars.get(String.valueOf(str.charAt(i))).setVisited(true);
+                uniqueChars.get(String.valueOf(str.charAt(i))).inrementCount();
                 count++;
             } else {
-                charset[(int)str.charAt(i)].inrementCount();
+                uniqueChars.get(String.valueOf(str.charAt(i))).inrementCount();
             }
             if (count == uniqueCharsLength) {
                 sb.append(str.charAt(i));
-                String optimizedStr = optimize(sb.toString(), charset);
-                System.out.println("Pattern found : " + sb.toString());
-                System.out.println("Optimized pattern: " + optimizedStr);
-                System.out.println("Rooms needed to visit: " + optimizedStr.length());
-                break;
+                String optimizedStr = optimize(sb.toString(), uniqueChars);
+//                System.out.println("Pattern found : " + sb.toString());
+//                System.out.println("Optimized pattern: " + optimizedStr);
+//                System.out.println("Rooms needed to visit: " + optimizedStr.length());
+                int skip = sb.toString().indexOf(optimizedStr);
+                start += (skip + 1);
+
+                patterns.add(optimizedStr);
+                if (optimizedStr.length() == uniqueCharsLength)
+                    break;
+                else
+                {
+                    i = start;
+                    sb.setLength(0);    //clearing sb
+                    count = 0;
+                    for (Node n : uniqueChars.values()) {
+                        n.resetCount();
+                        n.setVisited(false);
+                    }
+                    continue;
+                }
             }
             sb.append(str.charAt(i));
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (String pattern : patterns) {
+            if (pattern.length() < min)
+                min = pattern.length();
+        }
+
+        System.out.println(min);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int n = Integer.parseInt(scanner.nextLine());
+            String str = scanner.nextLine();
+            findMinRooms(str);
         }
 
     }
